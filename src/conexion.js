c419db663c71f5ad338d3f79b71f80814d308b92
GloriaMariscal <167749858+GloriaMariscal@users.noreply.github.com>
2025-05-15 20:39:06 +0200

    conexion

--- src/conexion.js
+++ src/conexion.js
@@ -1,21 +1,3 @@
-/*const mysql = require('mysql');
-
-// MySQL Connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root12',
-    database: 'red_social',
-});
-
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database');
-});
-
-module.exports = connection;*/
-
 const mysql = require('mysql');
 
 // Conexión a MySQL usando variables de entorno de Railway
